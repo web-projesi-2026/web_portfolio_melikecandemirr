aef784d0f6a0fbb240c5a3baa2ff7e94cd0be613
Melike <melikecandemir86@gmail.com>
2026-03-25 01:34:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tech-Timeline | Ahi Evran Üniversitesi Dijital Müze</title>
     
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assests/css/style.css">
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
